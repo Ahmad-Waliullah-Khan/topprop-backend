5900379b28c42ac7f7611a66c535d470acb9887c
Oswaldo Becerra <oswaldo@geekbears.com>
2020-08-10 15:44:06 +0000

    Ob #8u9xwx players

--- src/models/player.model.ts
+++ src/models/player.model.ts
@@ -23,6 +23,162 @@ export class Player extends Base {
     })
     position: string;
 
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc2: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc3: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc4: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc5: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc6: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc7: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc8: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc9: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc10: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc11: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc12: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc13: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc14: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc15: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc16: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc17: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc18: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc19: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc20: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc21: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc22: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc23: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc24: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc25: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc26: number;
+
+    @property({
+        type: 'number',
+        required: false,
+    })
+    bc27: number;
+
     @belongsTo(() => Team)
     teamId: number;
 
