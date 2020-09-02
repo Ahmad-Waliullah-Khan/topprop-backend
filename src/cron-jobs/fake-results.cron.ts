d76963a6227e1c2b7acb61d445f73fc803674827
Ivan Rangel <ivan05rangel@gmail.com>
2020-09-01 21:55:04 -0500

    CU-bpqk5y | Add fake reuslts cron

--- src/cron-jobs/fake-results.cron.ts
+++ src/cron-jobs/fake-results.cron.ts
@@ -1,7 +1,13 @@
 import { CronJob, cronJob } from '@loopback/cron';
 import { repository } from '@loopback/repository';
-import { ContenderRepository, ContestRepository, PlayerResultRepository } from '@src/repositories';
-import { CONTEST_STATUSES, CONTEST_TYPES } from '@src/utils/constants';
+import {
+    BetRepository,
+    ContenderRepository,
+    ContestRepository,
+    GainRepository,
+    PlayerResultRepository,
+} from '@src/repositories';
+import { CONTEST_STATUSES, CONTEST_TYPES, CRON_JOBS } from '@src/utils/constants';
 import { isEqual } from 'lodash';
 import moment from 'moment';
 
@@ -11,16 +17,18 @@ export class FakeResultsCron extends CronJob {
         @repository('PlayerResultRepository') private playerResultRepository: PlayerResultRepository,
         @repository('ContestRepository') private contestRepository: ContestRepository,
         @repository('ContenderRepository') private contenderRepository: ContenderRepository,
+        @repository('BetRepository') private betRepository: BetRepository,
+        @repository('GainRepository') private gainRepository: GainRepository,
     ) {
         super({
-            cronTime: '0 0 * * * *', // Every hour
-            name: 'fake-results',
+            cronTime: '0 * * * * *', // Every hour
+            // cronTime: '0 0 * * * *', // Every hour
+            name: CRON_JOBS.FAKE_RESULTS_CRON,
             onTick: async () => {
                 const contests = await this.contestRepository.find({
                     where: { ended: false, status: { nin: [CONTEST_STATUSES.CLOSED, CONTEST_STATUSES.UNMATCHED] } },
                     include: [{ relation: 'contenders' }],
                 });
-                console.log(this.randomIntFromInterval(0, 60));
                 for (let index = 0; index < contests.length; index++) {
                     const contest = contests[index];
                     if (isEqual(contest.status, CONTEST_STATUSES.OPEN)) {
@@ -48,15 +56,44 @@ export class FakeResultsCron extends CronJob {
                         //* DEFINE WINNER AND LOOSER
                         for (let index = 0; index < contest.contenders.length; index++) {
                             const contender = contest.contenders[index];
-                            //TODO NEED TO CHECK EQUALITY
+                            // const bet = await this.betRepository.findOne({ where: { contenderId: contender.id } });
+                            //* TIED IF FANTASY POINTS AND RESULTS POINTS ARE EQUAL
+                            if (isEqual(results.points, fantasyPoints)) {
+                                await this.contenderRepository.updateById(contender.id, {
+                                    tied: true,
+                                    tiedAt: moment().toDate(),
+                                });
+                                console.log(`Contender tied updated.`);
+                                await this.gainRepository.create({
+                                    amount: +contender.toRiskAmount,
+                                    notes: `Contest tied`,
+                                    contenderId: contender.id,
+                                    userId: contender.contenderId,
+                                });
+                                console.log(`Gain created on contest tied`);
+                            }
                             if (isEqual(contender.type, CONTEST_TYPES.OVER) && results.points > fantasyPoints) {
-                                await this.contenderRepository.updateById(contender.id, { winner: true });
+                                await this.contenderRepository.updateById(contender.id, {
+                                    winner: true,
+                                    wonAt: moment().toDate(),
+                                });
                                 console.log(`Contender winner updated.`);
+                                await this.gainRepository.create({
+                                    contenderId: contender.id,
+                                    userId: contender.contenderId,
+                                    amount: +contender.toRiskAmount,
+                                });
+                                console.log(`Gain created on contest won. Return initial bet.`);
+                                await this.gainRepository.create({
+                                    contenderId: contender.id,
+                                    userId: contender.contenderId,
+                                    amount: +contender.toWinAmount,
+                                });
+                                console.log(`Gain created on contest won. Win amount.`);
                             }
                         }
                     }
                 }
-                // console.log(users);
             },
             start: true,
         });
