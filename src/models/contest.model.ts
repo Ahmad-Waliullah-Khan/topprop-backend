10fd72f1d71da9094b4a0e98be83c779b76801a1
glenn_ferns <glennferns93@gmail.com>
2021-05-26 21:18:54 +0530

    Changelog - Added relations for  - winner  - creator  - claimer  - claimer player  - creatorPlayer

--- src/models/contest.model.ts
+++ src/models/contest.model.ts
@@ -1,238 +1,239 @@
-import { belongsTo, hasMany, model, property } from '@loopback/repository';
+import { belongsTo, hasMany, model, property, hasOne } from '@loopback/repository';
 import { CONTEST_STAKEHOLDERS, CONTEST_STATUSES, CONTEST_TYPES } from '@src/utils/constants';
 import { Base } from './base.model';
 import { Contender } from './contender.model';
 import { Game } from './game.model';
-import { Player } from './player.model';
-import { User } from './user.model';
-import { Spread } from '@src/models/spread.model';
+import { Player, PlayerWithRelations } from './player.model';
+import { User, UserWithRelations } from './user.model';
+import { Spread, SpreadWithRelations } from '@src/models/spread.model';
 
 @model()
 export class Contest extends Base {
-    @property({
-        id: true,
-        generated: true,
-        type: 'Number',
-        description: 'The unique identifier for a contest',
-    })
-    id: number;
+  @property({
+    id: true,
+    generated: true,
+    type: 'Number',
+    description: 'The unique identifier for a contest',
+  })
+  id: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        description: 'Entry amount in dollars for the contest. Would be the same for creator and claimer',
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    entryAmount: number;
+  @property({
+    type: 'number',
+    required: true,
+    description: 'Entry amount in dollars for the contest. Would be the same for creator and claimer',
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  entryAmount: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        description: 'Projected Fantasy points for creator player at the time of contest creation. Posterity purposes ',
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    creatorPlayerProjFantasyPoints: number;
+  @property({
+    type: 'number',
+    required: true,
+    description: 'Projected Fantasy points for creator player at the time of contest creation. Posterity purposes ',
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  creatorPlayerProjFantasyPoints: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        description: 'Projected Fantasy points for claimer player at the time of contest creation. Posterity purposes ',
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    claimerPlayerProjFantasyPoints: number;
+  @property({
+    type: 'number',
+    required: true,
+    description: 'Projected Fantasy points for claimer player at the time of contest creation. Posterity purposes ',
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  claimerPlayerProjFantasyPoints: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        description: 'Cover for creator player at the time of contest creation',
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    creatorPlayerCover: number;
+  @property({
+    type: 'number',
+    required: true,
+    description: 'Cover for creator player at the time of contest creation',
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  creatorPlayerCover: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        description: 'Cover for claimer player at the time of contest creation',
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    claimerPlayerCover: number;
+  @property({
+    type: 'number',
+    required: true,
+    description: 'Cover for claimer player at the time of contest creation',
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  claimerPlayerCover: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        description: 'Win Bonus for creator player at the time of contest creation. Only >0 if winBonus flag is set',
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    creatorPlayerWinBonus: number;
+  @property({
+    type: 'number',
+    required: true,
+    description: 'Win Bonus for creator player at the time of contest creation. Only >0 if winBonus flag is set',
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  creatorPlayerWinBonus: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        description: 'Win Bonus for claimer player at the time of contest creation. Only >0 if winBonus flag is set',
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    claimerPlayerWinBonus: number;
+  @property({
+    type: 'number',
+    required: true,
+    description: 'Win Bonus for claimer player at the time of contest creation. Only >0 if winBonus flag is set',
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  claimerPlayerWinBonus: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        description: 'FP Spread for creator player at the time of contest creation',
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    creatorPlayerSpread: number;
+  @property({
+    type: 'number',
+    required: true,
+    description: 'FP Spread for creator player at the time of contest creation',
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  creatorPlayerSpread: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        description: 'FP Spread for claimer player at the time of contest creation',
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    claimerPlayerSpread: number;
+  @property({
+    type: 'number',
+    required: true,
+    description: 'FP Spread for claimer player at the time of contest creation',
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  claimerPlayerSpread: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    creatorPlayerMaxWin: number;
+  @property({
+    type: 'number',
+    required: true,
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  creatorPlayerMaxWin: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    claimerPlayerMaxWin: number;
+  @property({
+    type: 'number',
+    required: true,
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  claimerPlayerMaxWin: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    spreadValue: number;
+  @property({
+    type: 'number',
+    required: true,
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  spreadValue: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    mlValue: number;
+  @property({
+    type: 'number',
+    required: true,
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  mlValue: number;
 
-    @property({
-        type: 'string',
-        required: true,
-        default: CONTEST_TYPES.LOBBY,
-    })
-    type: CONTEST_TYPES;
+  @property({
+    type: 'string',
+    required: true,
+    default: CONTEST_TYPES.LOBBY,
+  })
+  type: CONTEST_TYPES;
 
-    @property({
-        type: 'string',
-        required: true,
-        default: CONTEST_STATUSES.OPEN,
-    })
-    status: CONTEST_STATUSES;
+  @property({
+    type: 'string',
+    required: true,
+    default: CONTEST_STATUSES.OPEN,
+  })
+  status: CONTEST_STATUSES;
 
-    @property({
-        type: 'boolean',
-        required: false,
-        default: false,
-    })
-    ended: boolean;
+  @property({
+    type: 'boolean',
+    required: false,
+    default: false,
+  })
+  ended: boolean;
 
-    @property({
-        type: 'date',
-        required: false,
-        default: null,
-    })
-    endedAt: Date | null;
+  @property({
+    type: 'date',
+    required: false,
+    default: null,
+  })
+  endedAt: Date | null;
 
-    @property({
-        type: 'string',
-        default: null,
-    })
-    winnerLabel: CONTEST_STAKEHOLDERS;
+  @property({
+    type: 'string',
+    default: null,
+  })
+  winnerLabel: CONTEST_STAKEHOLDERS;
 
-    @property({
-        type: 'number',
-        default: null,
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    creatorWinAmount: number;
+  @property({
+    type: 'number',
+    default: null,
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  creatorWinAmount: number;
 
-    @property({
-        type: 'number',
-        default: null,
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    claimerWinAmount: number;
+  @property({
+    type: 'number',
+    default: null,
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  claimerWinAmount: number;
 
-    @property({
-        type: 'number',
-        required: true,
-        default: 0,
-        postgresql: {
-            dataType: 'decimal',
-        },
-    })
-    topPropProfit: number;
+  @property({
+    type: 'number',
+    required: true,
+    default: 0,
+    postgresql: {
+      dataType: 'decimal',
+    },
+  })
+  topPropProfit: number;
 
-    @belongsTo(() => User)
-    winnerId: number;
+  @belongsTo(() => User)
+  winnerId: number;
 
-    @belongsTo(() => User)
-    creatorId: number;
+  @belongsTo(() => User)
+  creatorId: number;
 
-    @belongsTo(() => User)
-    claimerId: number;
+  @belongsTo(() => User)
+  claimerId: number;
 
-    @belongsTo(() => Spread)
-    spreadId: number;
+  @belongsTo(() => Player)
+  creatorPlayerId: number;
 
-    @belongsTo(() => Player)
-    creatorPlayerId: number;
+  @belongsTo(() => Player)
+  claimerPlayerId: number;
 
-    @belongsTo(() => Player)
-    claimerPlayerId: number;
+  @belongsTo(() => Spread)
+  spreadId: number;
 
-    constructor(data?: Partial<Contest>) {
-        super(data);
-    }
+  constructor(data?: Partial<Contest>) {
+    super(data);
+  }
 }
 
 export interface ContestRelations {
-    creator?: User;
-    creatorPlayer?: Player;
-    claimerPlayer?: Player;
-    spread?: Spread;
+    winner?: UserWithRelations;
+    creator?: UserWithRelations;
+    creatorPlayer?: PlayerWithRelations;
+    claimerPlayer?: PlayerWithRelations;
+    spread?: SpreadWithRelations;
 }
 
 export type ContestWithRelations = Contest & ContestRelations;
--- src/repositories/contest.repository.ts
+++ src/repositories/contest.repository.ts
@@ -1,12 +1,42 @@
-import { inject } from '@loopback/core';
+import { Getter, inject } from '@loopback/core';
 import moment from 'moment';
-import { DefaultCrudRepository } from '@loopback/repository';
+import { BelongsToAccessor, DefaultCrudRepository, repository } from '@loopback/repository';
 import { DbDataSource } from '../datasources';
-import { Contest, ContestRelations } from '../models';
+import { Contest, ContestRelations, Spread, User, Player } from '../models';
+
+import { UserRepository } from './user.repository';
+import { PlayerRepository } from './player.repository';
+import { SpreadRepository } from './spread.repository';
 
 export class ContestRepository extends DefaultCrudRepository<Contest, typeof Contest.prototype.id, ContestRelations> {
-    constructor(@inject('datasources.db') dataSource: DbDataSource) {
+    public readonly winner: BelongsToAccessor<User, typeof Contest.prototype.id>;
+    public readonly creator: BelongsToAccessor<User, typeof Contest.prototype.id>;
+    public readonly claimer: BelongsToAccessor<User, typeof Contest.prototype.id>;
+    public readonly creatorPlayer: BelongsToAccessor<Player, typeof Contest.prototype.id>;
+    public readonly claimerPlayer: BelongsToAccessor<Player, typeof Contest.prototype.id>;
+
+    constructor(
+        @inject('datasources.db') dataSource: DbDataSource,
+        @repository.getter('UserRepository') protected userRepositoryGetter: Getter<UserRepository>,
+        @repository.getter('PlayerRepository') protected playerRepositoryGetter: Getter<PlayerRepository>,
+        @repository.getter('SpreadRepository') protected spreadRepositoryGetter: Getter<SpreadRepository>,
+    ) {
         super(Contest, dataSource);
+        
+        this.winner = this.createBelongsToAccessorFor('winner', userRepositoryGetter);
+        this.registerInclusionResolver('winner', this.winner.inclusionResolver);
+
+        this.creator = this.createBelongsToAccessorFor('creator', userRepositoryGetter);
+        this.registerInclusionResolver('creator', this.creator.inclusionResolver);
+
+        this.claimer = this.createBelongsToAccessorFor('claimer', userRepositoryGetter);
+        this.registerInclusionResolver('claimer', this.creator.inclusionResolver);
+
+        this.creatorPlayer = this.createBelongsToAccessorFor('creatorPlayer', playerRepositoryGetter);
+        this.registerInclusionResolver('creatorPlayer', this.creatorPlayer.inclusionResolver);
+
+        this.claimerPlayer = this.createBelongsToAccessorFor('claimerPlayer', playerRepositoryGetter);
+        this.registerInclusionResolver('claimerPlayer', this.claimerPlayer.inclusionResolver);
 
         //* BEFORE SAVE HOOK
         //* ASSIGN UPDATED AT
