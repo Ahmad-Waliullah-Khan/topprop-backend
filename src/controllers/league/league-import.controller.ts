cb2e5bcce6649a919a884318d7bedf88f0d00e4d
Ahmad Khan <ahmad.waliullah.khan@gmail.com>
2021-07-19 20:27:39 +0530

    Access and Refresh tokens are included in fetch yahoo api response

--- src/controllers/league/league-import.controller.ts
+++ src/controllers/league/league-import.controller.ts
@@ -1,21 +1,20 @@
-import { authenticate } from '@loopback/authentication';
-import { authorize } from '@loopback/authorization';
-import { service } from '@loopback/core';
-import { HttpErrors, post, requestBody } from '@loopback/rest';
-import { LeagueService } from '@src/services/league.service';
-import { API_ENDPOINTS, PERMISSIONS } from '@src/utils/constants';
-import { ErrorHandler } from '@src/utils/helpers';
-import { AuthorizationHelpers } from '@src/utils/helpers/authorization.helpers';
-import { ICommonHttpResponse } from '@src/utils/interfaces';
+import {authenticate} from '@loopback/authentication';
+import {authorize} from '@loopback/authorization';
+import {service} from '@loopback/core';
+import {HttpErrors, post, requestBody} from '@loopback/rest';
+import {LeagueService} from '@src/services/league.service';
+import {API_ENDPOINTS, PERMISSIONS} from '@src/utils/constants';
+import {ErrorHandler} from '@src/utils/helpers';
+import {AuthorizationHelpers} from '@src/utils/helpers/authorization.helpers';
+import {ICommonHttpResponse} from '@src/utils/interfaces';
 import {
-    ILeaguesImportRequestYahoo,
     ILeagueImportRequestEspn,
-    ILeagueImportRequestYahoo,
+    ILeagueImportRequestYahoo, ILeaguesImportRequestYahoo
 } from '@src/utils/interfaces/league-import.interface';
-import { COMMON_MESSAGES, LEAGUE_IMPORT_MESSAGES } from '@src/utils/messages';
-import { FETCH_LEAGUE_VALIDATOR } from '@src/utils/validators/league-import.validators';
+import {COMMON_MESSAGES, LEAGUE_IMPORT_MESSAGES} from '@src/utils/messages';
+import {FETCH_LEAGUE_VALIDATOR} from '@src/utils/validators/league-import.validators';
 // import {Client} from 'espn-fantasy-football-api/node';
-import { isEmpty } from 'lodash';
+import {isEmpty} from 'lodash';
 import Schema from 'validate';
 const { Client } = require('espn-fantasy-football-api/node-dev');
 const YahooFantasy = require('yahoo-fantasy');
@@ -55,9 +54,15 @@ export class LeagueImportController {
 
             const leagues = await yf.user.game_leagues('nfl');
 
+            const tokens = {
+                "accessToken": access_token,
+                "refreshToken": refresh_token
+            }
+
             return {
                 message: LEAGUE_IMPORT_MESSAGES.FETCH_SUCCESS,
                 data: {
+                    tokens: tokens,
                     leagues: leagues,
                 },
             };
