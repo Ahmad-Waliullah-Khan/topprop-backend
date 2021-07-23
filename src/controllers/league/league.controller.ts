4c2cafed014cfdd6a2ce1e712aaa9ab5619598d5
Ahmad Khan <ahmad.waliullah.khan@gmail.com>
2021-07-24 01:46:31 +0530

    League Contest Creation
    /league/create-contest api

--- src/controllers/league/league.controller.ts
+++ src/controllers/league/league.controller.ts
@@ -1,17 +1,19 @@
 import {authenticate} from '@loopback/authentication';
 import {authorize} from '@loopback/authorization';
-import {inject} from '@loopback/core';
+import {inject, service} from '@loopback/core';
 import {repository} from '@loopback/repository';
 import {get, getModelSchemaRef, HttpErrors, post, requestBody} from '@loopback/rest';
 import {SecurityBindings, securityId} from '@loopback/security';
-import {League} from '@src/models';
-import {ContestRosterRepository, LeagueRepository, MemberRepository, PlayerRepository, RosterRepository, TeamRepository} from '@src/repositories';
-import {API_ENDPOINTS, PERMISSIONS} from '@src/utils/constants';
+import {Bet, ContestRoster, League, LeagueContest} from '@src/models';
+import {BetRepository, ContestRosterRepository, LeagueContestRepository, LeagueRepository, MemberRepository, PlayerRepository, RosterRepository, TeamRepository, UserRepository} from '@src/repositories';
+import {UserService, WalletService} from '@src/services';
+import {LeagueService} from '@src/services/league.service';
+import {API_ENDPOINTS, CONTEST_STATUSES, PERMISSIONS, SPREAD_TYPE} from '@src/utils/constants';
 import {ErrorHandler} from '@src/utils/helpers';
 import {AuthorizationHelpers} from '@src/utils/helpers/authorization.helpers';
 import {ICommonHttpResponse, ICustomUserProfile} from '@src/utils/interfaces';
 import {ILeagueCreateRequest} from '@src/utils/interfaces/league.interface';
-import {COMMON_MESSAGES, LEAGUE_IMPORT_MESSAGES, LEAGUE_MESSAGES} from '@src/utils/messages';
+import {COMMON_MESSAGES, CONTEST_MESSAGES, LEAGUE_IMPORT_MESSAGES, LEAGUE_MESSAGES} from '@src/utils/messages';
 import {LEAGUE_CONTEST_VALIDATOR} from '@src/utils/validators';
 import {isEmpty} from 'lodash';
 import Schema from 'validate';
@@ -20,6 +22,8 @@ export class LeagueController {
     constructor(
         @repository(LeagueRepository)
         public leagueRepository: LeagueRepository,
+        @repository(LeagueContestRepository)
+        public leagueContestRepository: LeagueContestRepository,
         @repository(MemberRepository)
         public memberRepository: MemberRepository,
         @repository(TeamRepository)
@@ -30,6 +34,13 @@ export class LeagueController {
         public rosterRepository: RosterRepository,
         @repository(ContestRosterRepository)
         public contestRosterRepository: ContestRosterRepository,
+        @repository(BetRepository)
+        public betRepository: BetRepository,
+        @repository(UserRepository)
+        public userRepository: UserRepository,
+        @service() private leagueService: LeagueService,
+        @service() private walletService: WalletService,
+        @service() private userService: UserService,
 
     ) {}
 
@@ -104,12 +115,14 @@ export class LeagueController {
         const validationErrors = validation.validate(body);
         if (validationErrors.length) throw new HttpErrors.BadRequest(ErrorHandler.formatError(validationErrors));
 
-        const { creatorTeamId, claimerTeamId, entryAmount, winBonus, creatorId } = body;
+        const creatorTeamId = body.creatorTeamId || 0;
+        const claimerTeamId = body.claimerTeamId || 0;
+        const creatorId = body.creatorId || 0;
 
-        const creatorTeam = await this.teamRepository.findById(Number(creatorTeamId));
+        const creatorTeam = await this.teamRepository.findById(creatorTeamId);
         if (!creatorTeam) throw new HttpErrors.BadRequest(LEAGUE_MESSAGES.CREATOR_TEAM_DOES_NOT_EXIST);
 
-        const claimerTeam = await this.teamRepository.findById(Number(claimerTeamId));
+        const claimerTeam = await this.teamRepository.findById(claimerTeamId);
         if (!claimerTeam) throw new HttpErrors.BadRequest(LEAGUE_MESSAGES.CLAIMER_TEAM_DOES_NOT_EXIST);
 
         const member = await this.memberRepository.find({
@@ -124,31 +137,219 @@ export class LeagueController {
 
         if(creatorTeam.leagueId !== claimerTeam.leagueId) throw new HttpErrors.BadRequest(LEAGUE_MESSAGES.NOT_SAME_LEAGUE);
 
+        try {
 
-        //Do contest calculation
-        //Clone roster to contest-roster
-        //Save DB
+            const creatorPlayers = creatorTeam.players;
+            const claimerPlayers = claimerTeam.players;
+
+            const remainingCreatorPlayers = creatorPlayers.filter((player) => {
+                return !player?.isOver;
+            });
+
+            const remainingClaimerPlayers = claimerPlayers.filter((player) => {
+                return !player?.isOver;
+            });
+
+            const creatorTeamPlayerProjFantasy = remainingCreatorPlayers.map((player) => {
+                return player.projectedFantasyPoints;
+            });
+
+            const claimerTeamPlayerProjFantasy = remainingClaimerPlayers.map((player) => {
+                return player.projectedFantasyPoints;
+            });
+
+            let totalCreatorTeamProjFantasy = 0;
+            for(const i in creatorTeamPlayerProjFantasy) { totalCreatorTeamProjFantasy += creatorTeamPlayerProjFantasy[i]; }
+
+            let totalClaimerTeamProjFantasy = 0;
+            for(const i in claimerTeamPlayerProjFantasy) { totalClaimerTeamProjFantasy += claimerTeamPlayerProjFantasy[i]; }
+
+            const funds = await this.walletService.userBalance(+currentUser[securityId]);
+            const entryAmount = body.entryAmount || 0;
+            if (funds < entryAmount * 100) throw new HttpErrors.BadRequest(CONTEST_MESSAGES.INSUFFICIENT_BALANCE);
+
+            const winBonusFlag = body.winBonus || false;
+
+            const creatorTeamSpread = await this.leagueService.calculateSpread(
+                totalCreatorTeamProjFantasy,
+                totalClaimerTeamProjFantasy,
+                'creator',
+            );
+            const claimerTeamSpread = await this.leagueService.calculateSpread(
+                totalCreatorTeamProjFantasy,
+                totalClaimerTeamProjFantasy,
+                'claimer',
+            );
+
+            let creatorTeamCover = 0;
+            let claimerTeamCover = 0;
+            let creatorTeamWinBonus = 0;
+            let claimerTeamWinBonus = 0;
+
+            if((remainingClaimerPlayers.length<=2) || (remainingCreatorPlayers.length<=2)) {
+                creatorTeamCover = await this.leagueService.calculateCover(
+                    creatorTeamSpread,
+                    entryAmount,
+                    winBonusFlag,
+                    SPREAD_TYPE.LEAGUE_1_TO_2
+                );
+
+                claimerTeamCover = await this.leagueService.calculateCover(
+                    claimerTeamSpread,
+                    entryAmount,
+                    winBonusFlag,
+                    SPREAD_TYPE.LEAGUE_1_TO_2
+                );
+
+                creatorTeamWinBonus = winBonusFlag
+                ? await this.leagueService.calculateWinBonus(creatorTeamSpread, entryAmount, SPREAD_TYPE.LEAGUE_1_TO_2)
+                : 0;
+                claimerTeamWinBonus = winBonusFlag
+                ? await this.leagueService.calculateWinBonus(claimerTeamSpread, entryAmount, SPREAD_TYPE.LEAGUE_1_TO_2)
+                : 0;
+            }
+
+            if((remainingClaimerPlayers.length>2) && (remainingClaimerPlayers.length<=6) || (remainingCreatorPlayers.length>2) && (remainingCreatorPlayers.length<=6)) {
+                creatorTeamCover = await this.leagueService.calculateCover(
+                    creatorTeamSpread,
+                    entryAmount,
+                    winBonusFlag,
+                    SPREAD_TYPE.LEAGUE_3_TO_6
+                );
+
+                claimerTeamCover = await this.leagueService.calculateCover(
+                    claimerTeamSpread,
+                    entryAmount,
+                    winBonusFlag,
+                    SPREAD_TYPE.LEAGUE_3_TO_6
+                );
+
+                creatorTeamWinBonus = winBonusFlag
+                ? await this.leagueService.calculateWinBonus(creatorTeamSpread, entryAmount, SPREAD_TYPE.LEAGUE_3_TO_6)
+                : 0;
+                claimerTeamWinBonus = winBonusFlag
+                ? await this.leagueService.calculateWinBonus(claimerTeamSpread, entryAmount, SPREAD_TYPE.LEAGUE_3_TO_6)
+                : 0;
+            }
+
+            if((remainingClaimerPlayers.length>=7) && (remainingClaimerPlayers.length<=18) || (remainingCreatorPlayers.length>=7) && (remainingCreatorPlayers.length<=18)) {
+                creatorTeamCover = await this.leagueService.calculateCover(
+                    creatorTeamSpread,
+                    entryAmount,
+                    winBonusFlag,
+                    SPREAD_TYPE.LEAGUE_7_TO_18
+                );
+
+                claimerTeamCover = await this.leagueService.calculateCover(
+                    claimerTeamSpread,
+                    entryAmount,
+                    winBonusFlag,
+                    SPREAD_TYPE.LEAGUE_7_TO_18
+                );
+
+                creatorTeamWinBonus = winBonusFlag
+                ? await this.leagueService.calculateWinBonus(creatorTeamSpread, entryAmount, SPREAD_TYPE.LEAGUE_7_TO_18)
+                : 0;
+                claimerTeamWinBonus = winBonusFlag
+                ? await this.leagueService.calculateWinBonus(claimerTeamSpread, entryAmount, SPREAD_TYPE.LEAGUE_7_TO_18)
+                : 0;
+            }
+
+            const creatorTeamMaxWin = Number(creatorTeamCover) + Number(creatorTeamWinBonus);
+            const claimerTeamMaxWin = Number(claimerTeamCover) + Number(claimerTeamWinBonus);
+
+            const spreadValue = entryAmount * 0.85;
+            const mlValue = entryAmount - spreadValue;
+
+            const leagueContestData = new LeagueContest();
+            const userId = body.creatorId;
+
+            leagueContestData.creatorId = userId;
+            leagueContestData.creatorTeamId = creatorTeamId;
+            leagueContestData.claimerTeamId = claimerTeamId;
+            leagueContestData.entryAmount = entryAmount;
+            leagueContestData.creatorPlayerProjFantasyPoints = totalCreatorTeamProjFantasy;
+            leagueContestData.claimerPlayerProjFantasyPoints = totalClaimerTeamProjFantasy;
+            leagueContestData.claimerTeamCover = creatorTeamCover;
+            leagueContestData.claimerTeamCover = claimerTeamCover;
+            leagueContestData.creatorTeamMaxWin = creatorTeamMaxWin;
+            leagueContestData.claimerTeamMaxWin = claimerTeamMaxWin;
+            leagueContestData.creatorTeamWinBonus = creatorTeamWinBonus;
+            leagueContestData.claimerTeamWinBonus = claimerTeamWinBonus;
+            leagueContestData.creatorTeamSpread = creatorTeamSpread;
+            leagueContestData.claimerTeamSpread = claimerTeamSpread;
+            leagueContestData.spreadValue = spreadValue;
+            leagueContestData.mlValue = mlValue;
+            leagueContestData.status = CONTEST_STATUSES.OPEN;
+            leagueContestData.ended = false;
+
+            const createdLeagueContest = await this.leagueContestRepository.create(leagueContestData);
+
+            creatorTeam.players.map(async (player: any) => {
+                const contestRosterData = new ContestRoster();
+                contestRosterData.teamId = creatorTeamId;
+                contestRosterData.playerId = player.id;
+                await this.contestRosterRepository.create(contestRosterData,);
+                return false;
+            });
+
+            claimerTeam.players.map(async (player: any) => {
+                const contestRosterData = new ContestRoster();
+                contestRosterData.teamId = claimerTeamId;
+                contestRosterData.playerId = player.id;
+                await this.contestRosterRepository.create(contestRosterData,);
+                return false;
+            });
+
+            const bet = new Bet();
+
+            bet.contenderId = creatorTeamId;
+            bet.userId = userId;
+            bet.amount = entryAmount * 100;
+
+            await this.betRepository.create(bet);
+
+            const myContestFilter = {
+                where: {
+                    and: [
+                        { ended: false },
+                        { or: [{ creatorId: userId }, { claimerId: userId }] },
+                        {
+                            or: [
+                                { status: CONTEST_STATUSES.OPEN },
+                                { status: CONTEST_STATUSES.MATCHED },
+                                { status: CONTEST_STATUSES.UNMATCHED },
+                            ],
+                        },
+                    ],
+                },
+                include: ['creator', 'claimer', 'winner', 'creatorTeam', 'claimerTeam'],
+            };
+
+            const contestFilter = {
+                where: {
+                    status: CONTEST_STATUSES.OPEN,
+                    ended: false,
+                    creatorId: { neq: userId },
+                },
+                include: ['creator', 'claimer', 'winner', 'creatorTeam', 'claimerTeam'],
+            };
+            const myContests = await this.leagueContestRepository.find(myContestFilter);
+            const contests = await this.leagueContestRepository.find(contestFilter);
 
 
+            return {
+                message: LEAGUE_MESSAGES.CREATE_LEAGUE_CONTEST_SUCCESS,
+                data: {
+                    myContests: myContests,
+                    contests: contests,
+                },
+            };
 
-        // fetch the creatorTeam along with players
-        // fetch the claimerTeam along with players
-        // find the number of players available in the creator team
-        // find the number of players available in the claimer team
-        // if any of the team contains number of available players <=2 then first sheet calculation
-        // if any of the team contains number of available players >=3 and <=6 then second sheet calculation
-        // if any of the team contains number of available players >=7 and <=18 then third sheet calculation
-        // calculate total projectedFantasy of the players of both team which will be team's projected fantasy
-        // Calculate the spread according to spread table
-        // calculate winBonus if boolean is set
-        // calculate cover
-        // calculate maxWin
-        //
-
-        return {
-            message: LEAGUE_MESSAGES.CREATE_LEAGUE_CONTEST_SUCCESS,
-            data: {},
-        };
-
+        }
+        catch (error) {
+            console.log(error);
+            throw new HttpErrors.BadRequest(LEAGUE_MESSAGES.CREATE_LEAGUE_CONTEST_FAILED,);
+        }
     }
 }
