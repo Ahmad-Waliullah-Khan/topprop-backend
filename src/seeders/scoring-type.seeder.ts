c10746d34b9a5ab9bf6ebfd87cdc5f6113913dc6
Ahmad Khan <ahmad.waliullah.khan@gmail.com>
2021-07-31 03:32:28 +0530

    Uncommented seeder

--- src/seeders/scoring-type.seeder.ts
+++ src/seeders/scoring-type.seeder.ts
@@ -1,14 +1,14 @@
-// export const ScoringTypeSeeder = [
-//     {
-//         id: 1,
-//         name: 'Half PPR',
-//     },
-//     {
-//         id: 2,
-//         name: 'Full PPR',
-//     },
-//     {
-//         id: 3,
-//         name: 'Standard',
-//     },
-// ];
+export const ScoringTypeSeeder = [
+    {
+        id: 1,
+        name: 'Half PPR',
+    },
+    {
+        id: 2,
+        name: 'Full PPR',
+    },
+    {
+        id: 3,
+        name: 'Standard',
+    },
+];
