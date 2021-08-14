46ce0f1b29f93e8a280cd885a45c2688ea1c0da5
Subir <subir@viabl.ventures>
2021-08-14 18:57:27 +0530

    Save logs to /var/logs

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -1,4 +1,9 @@
+const path = require('path');
 const winston = require('winston');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const LOGS_DIR = isDevelopment ? 'logs' : '/var/logs';
 
 const logger = winston.createLogger({
     level: 'info',
@@ -9,20 +14,22 @@ const logger = winston.createLogger({
         // - Write to all logs with level `info` and below to `info.log`
         // - Write all logs error (and below) to `error.log`.
         //
-        new winston.transports.File({ filename: 'logs/debug.log', level: 'debug' }),
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/info.log' })
-    ]
+        new winston.transports.File({ filename: path.join(LOGS_DIR, 'debug.log'), level: 'debug' }),
+        new winston.transports.File({ filename: path.join(LOGS_DIR, 'error.log'), level: 'error' }),
+        new winston.transports.File({ filename: path.join(LOGS_DIR, 'info.log') }),
+    ],
 });
 
 //
 // If not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 //
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-      format: winston.format.simple(),
-    }));
-  }
+if (isDevelopment) {
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        }),
+    );
+}
 
 module.exports = logger;
