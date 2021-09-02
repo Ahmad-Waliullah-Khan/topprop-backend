5f2bc702ffdc3cd5cfb7897ca13a7466e8bdcc8e
Ahmad Khan <ahmad.waliullah.khan@gmail.com>
2021-09-03 00:47:56 +0530

    wip: fix for sync failure email for missing players

--- src/services/league.service.ts
+++ src/services/league.service.ts
@@ -507,7 +507,7 @@ export class LeagueService {
         }
 
         if (notFoundPlayers.length > 0) {
-            await this.handleSyncFail(localLeagueId, 'YAHOO');
+            await this.handleMissingPlayer(localLeagueId, 'YAHOO', notFoundPlayers);
         }
 
         return true;
@@ -774,7 +774,7 @@ export class LeagueService {
         }
 
         if(notFoundPlayers.length > 0) {
-            await this.handleSyncFail(localLeagueId, 'ESPN');
+            await this.handleMissingPlayer(localLeagueId, 'ESPN', notFoundPlayers);
         }
         return true;
     }
@@ -800,6 +800,32 @@ export class LeagueService {
 
             await this.userService.sendEmail(localLeague.userId, EMAIL_TEMPLATES.ADMIN_SYNC_FAILED, templateData);
         }
+    }
+
+    async handleMissingPlayer(leagueId: number, sourceName: string, playerList: any,) {
+        const localLeague = await this.leagueRepository.findById(Number(leagueId));
+
+        const missingPlayerNames = playerList.map((player: any) => {
+            return player?.playerPoolEntry?.player.firstName +" " +player?.playerPoolEntry?.player.lastName;
+        });
+        const templateData = {
+            user: {
+                fullName: 'Admin',
+            },
+            leagueId: leagueId,
+            sourceName: sourceName,
+            playerList,
+            text: {
+                title: 'Player Not Found.',
+                subtitle: `Some player are not found in TopProp system, for league - ${localLeague.name}
+                Following players are missing from the system: ${missingPlayerNames.toString()}`,
+            },
+        };
+
+        const adminEmail = process.env.SUPPORT_EMAIL_ADDRESS ? process.env.SUPPORT_EMAIL_ADDRESS : localLeague.user?.email;
+
+        await this.userService.sendEmail(localLeague.userId, EMAIL_TEMPLATES.ADMIN_SYNC_FAILED_PLAYER_NOT_FOUND, templateData, adminEmail);
+
     }
 
     async invalidateLeagueSync(leagueId: number) {}
