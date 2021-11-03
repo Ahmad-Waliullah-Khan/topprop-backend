c4b30bef24df086b30f571753d5a55e00f1b5b6c
Ahmad Khan <ahmad.waliullah.khan@gmail.com>
2021-11-04 00:45:12 +0530

    Added the script to misc cron

--- src/cron-jobs/miscellaneous.cron.ts
+++ src/cron-jobs/miscellaneous.cron.ts
@@ -1,10 +1,10 @@
-import { service } from '@loopback/core';
-import { CronJob, cronJob } from '@loopback/cron';
-import { CronService, MiscellaneousService } from '@src/services';
-import { CRON_JOBS } from '@src/utils/constants';
+import {service} from '@loopback/core';
+import {CronJob, cronJob} from '@loopback/cron';
+import {CronService, MiscellaneousService} from '@src/services';
+import {CRON_JOBS} from '@src/utils/constants';
 import chalk from 'chalk';
 import cron from 'cron';
-import { MISCELLANEOUS_TIMING } from '../utils/cron-timings';
+import {MISCELLANEOUS_TIMING} from '../utils/cron-timings';
 import logger from '../utils/logger';
 
 @cronJob()
@@ -17,7 +17,8 @@ export class MiscellaneousCron extends CronJob {
             onTick: async () => {
                 try {
                     // await this.miscellaneousService.resetNoPPRGradedContests();
-                    // this.cronService.cronLogger(CRON_JOBS.MISCELLANEOUS_CRON);
+                    await this.miscellaneousService.updateDOB();
+                    this.cronService.cronLogger(CRON_JOBS.MISCELLANEOUS_CRON);
 
                     const updatedCronTiming = await this.cronService.updatedCronConfig(CRON_JOBS.MISCELLANEOUS_CRON);
                     const updatedCronTime = new cron.CronTime(updatedCronTiming);
