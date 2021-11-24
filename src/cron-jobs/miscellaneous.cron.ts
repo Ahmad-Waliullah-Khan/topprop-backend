9938d1a5d42e3c6226b196709117b57b3d14d145
Vishal Sahu <vvishal.online@gmail.com>
2021-11-24 23:53:04 +0530

    Fix: Reset all players
    Squashed commit of the following:
    
    commit 3de781b8a836ee8b6508babc810ee1a790ab15eb
    Author: Ahmad Khan <ahmad.waliullah.khan@gmail.com>
    Date:   Wed Nov 24 23:42:53 2021 +0530
    
        Misc cron added for resetting isOver and hasStarted flags for players

--- src/cron-jobs/miscellaneous.cron.ts
+++ src/cron-jobs/miscellaneous.cron.ts
@@ -1,10 +1,10 @@
-import { service } from '@loopback/core';
-import { CronJob, cronJob } from '@loopback/cron';
-import { CronService, MiscellaneousService } from '@src/services';
-import { CRON_JOBS } from '@src/utils/constants';
+import {service} from '@loopback/core';
+import {CronJob, cronJob} from '@loopback/cron';
+import {CronService, MiscellaneousService} from '@src/services';
+import {CRON_JOBS} from '@src/utils/constants';
 import chalk from 'chalk';
 import cron from 'cron';
-import { MISCELLANEOUS_TIMING } from '../utils/cron-timings';
+import {MISCELLANEOUS_TIMING} from '../utils/cron-timings';
 import logger from '../utils/logger';
 
 @cronJob()
@@ -25,6 +25,7 @@ export class MiscellaneousCron extends CronJob {
                     // await this.miscellaneousService.updateDOB();
                     // await this.miscellaneousService.resetAllPlayers();
                     // await this.miscellaneousService.updateBonusPayoutProcessed();
+                    await this.miscellaneousService.makeAllPlayersAvailable();
                     const updatedCronTiming = await this.cronService.updatedCronConfig(CRON_JOBS.MISCELLANEOUS_CRON);
                     const updatedCronTime = new cron.CronTime(updatedCronTiming);
                     this.setTime(updatedCronTime);
